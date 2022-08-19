47219deeb55815ed6b9fb893769ff302cbfcb9cd
Philip Hell <blvckbytes@gmail.com>
2022-08-19 22:58:43 +0200

    added a shortened version of the file path to search's output table

--- commands/search.py
+++ commands/search.py
@@ -30,6 +30,17 @@ def find_matches(path, search):
   return list(filter(lambda file: does_path_match(file, search), files))
 
 '''
+Shorten an absolute java file path to a relative path, relative
+to the work directory of BuildTools, in order to save screen space.
+
+Example:
+/Users/blvckbytes/Documents/Tools/BuildTools/work/decompile-1ae486b6/... -> decompile-1ae486b6/...
+'''
+def shorten_path(path):
+  marker = '/work'
+  return path[path.index(marker) + len(marker) + 1:]
+
+'''
 Main entry point of this command
 '''
 def invoke(existing_versions, identifier_to_version, args):
@@ -52,10 +63,10 @@ def invoke(existing_versions, identifier_to_version, args):
         if jcl.not_a_class == True:
           continue
 
-        table.append([version, jcl.package, jcl.class_name, ', '.join(jcl.fields)])
+        table.append([version, jcl.package, jcl.class_name, ', '.join(jcl.fields), shorten_path(jcl.path)])
 
   print()
   print(tabulate.tabulate(
-    table, headers=['version', 'package', 'class', 'fields'],
+    table, headers=['version', 'package', 'class', 'fields', 'path'],
     tablefmt='pretty', numalign='left', stralign='left'
   ))
